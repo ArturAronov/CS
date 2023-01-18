87adfdbf890ca634fe166b2d70d5ef2e32b8f5ca
arturaronov <unionco@icloud.com>
2023-01-18 17:34:28 +0800

    initial commit

---
+++ beginner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Example 1: Hello World
+// https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-beginner-c-sharp/modules/csharp-hello-world/lessons/csharp-hello-world/exercises/run-some-c-sharp
+
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("Hello World!");
+//     }
+//   }
+// }
+
+
+// Example 2: Getting Input
+// https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-beginner-c-sharp/modules/csharp-hello-world/lessons/csharp-hello-world/exercises/getting-input
+
+// namespace GettingInput
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("How old are you?");
+//       string input = Console.ReadLine();
+//       Console.WriteLine($"Your are {input} years old!");
+//     }
+//   }
+// }
