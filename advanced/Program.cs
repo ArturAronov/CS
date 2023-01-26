fe704b940e36ab607bf636d9125034622671121d
arturaronov <unionco@icloud.com>
2023-01-26 14:17:18 +0800

    added Arrays examples

---
+++ advanced/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Example 1: Arrays
+// https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-intermediate-c-sharp/modules/learn-csharp-arrays-and-loops/lessons/csharp-arrays/exercises/built-in-methods
+
+// namespace BuiltInMethods
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       string[] summerStrut;
+//       summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+//       int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
+//       int threeStarSong = Array.IndexOf(ratings, 3);
+//
+//       Console.WriteLine($"Song number {threeStarSong + 1} is rated three stars");
+//       Console.WriteLine(Array.Find(summerStrut, element => element.Length >= 10));
+//
+//       Array.Sort(summerStrut);
+//       Console.WriteLine(summerStrut[0]);
+//       Console.WriteLine(summerStrut[7]);
+//     }
+//   }
+// }
+
+
+
