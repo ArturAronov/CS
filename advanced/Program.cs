f4458620028d62b0d54bf1f1ed443918d48ab427
arturaronov <unionco@icloud.com>
2023-01-26 14:36:33 +0800

    added array methods

--- advanced/Program.cs
+++ advanced/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-// Example 1: Arrays
+// Example 1: Arrays:
 // https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-intermediate-c-sharp/modules/learn-csharp-arrays-and-loops/lessons/csharp-arrays/exercises/built-in-methods
 
 // namespace BuiltInMethods
@@ -25,4 +25,27 @@
 // }
 
 
+// Example 2: Array Methods:
+// https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-intermediate-c-sharp/modules/learn-csharp-arrays-and-loops/lessons/csharp-arrays/exercises/documentation-hunt
 
+// namespace DocumentationHunt
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       string[] summerStrut;
+//       summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+//       int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
+//       string[] summerStrutCopy = new string[8];
+//
+//       Array.Copy(summerStrut, summerStrutCopy, 8);
+//       Array.Reverse(summerStrut);
+//       Array.Clear(ratings, 0, 8);
+//
+//       Console.WriteLine(summerStrutCopy[0]);
+//       Console.WriteLine(summerStrut[0]);
+//       Console.WriteLine(ratings[0]);
+//     }
+//   }
+// }
